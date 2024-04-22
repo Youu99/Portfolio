5d284df10b199ba7833ede2a1c8e789504851834
Youu99 <154948743+Youu99@users.noreply.github.com>
2024-04-22 17:36:37 +0900

    2024-04-22

---
+++ js/jquery.js
@@ -0,0 +1,33 @@
+$(function () {
+    $('#click_box').click(function () {
+        $('#title_page').fadeOut()
+        $('#swiper_page').css('display','block')
+    })
+})
+
+// SKILLS 태그 바
+
+$(function(){
+    
+    
+    $('.SKILLS_list').click(function(){
+        let idx= $(this).index();
+        $('.SKILLS_tag_box').eq(idx).fadeIn()
+    
+    })
+    $('.SKILLS_b').click(function(){
+        $('.SKILLS_tag_box').fadeOut()
+    })
+    $('.SKILLS_list2').click(function(){
+        let idx= $(this).index();
+        $('.SKILLS_tag_box2').eq(idx).fadeIn()
+    
+    })
+    $('.SKILLS_b').click(function(){
+        $('.SKILLS_tag_box2').fadeOut()
+    })
+})
+
+
+
+

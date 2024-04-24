93df09a90a75a4087770b5de4fc1c53bfa8ad815
Youu99 <154948743+Youu99@users.noreply.github.com>
2024-04-24 17:01:17 +0900

    2024-04-24

--- js/jquery.js
+++ js/jquery.js
@@ -8,27 +8,25 @@ $(function () {
 // SKILLS 태그 바
 
 $(function(){
-    
-    
     $('.SKILLS_list').click(function(){
-        let idx= $(this).index();
-        $('.SKILLS_tag_box').eq(idx).fadeIn()
-    
-    })
+        let idx = $(this).index(); // 클릭된 아이콘의 인덱스를 구함
+        if ($('.SKILLS_tag_box').eq(idx).is(":visible")) {
+            // 해당 창이 이미 보이는 경우, 창을 숨김
+            $('.SKILLS_tag_box').eq(idx).slideUp();
+        } else {
+            // 해당 창이 보이지 않는 경우, 다른 모든 창을 숨기고 클릭된 창만 보임
+            $('.SKILLS_tag_box').slideUp();
+            $('.SKILLS_tag_box').eq(idx).slideDown();
+        }
+    });
     $('.SKILLS_b').click(function(){
-        $('.SKILLS_tag_box').fadeOut()
-    })
-    $('.SKILLS_list2').click(function(){
-        let idx= $(this).index();
-        $('.SKILLS_tag_box2').eq(idx).fadeIn()
-    
-    })
-    $('.SKILLS_b').click(function(){
-        $('.SKILLS_tag_box2').fadeOut()
-    })
-})
+        $('.SKILLS_tag_box').slideUp(); // 모든 창을 숨김
+    });
+});
 
 
+
+// 텍스트 스크롤 애니메이션 
 
 
 
--- js/swiper.js
+++ js/swiper.js
@@ -5,9 +5,45 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     pagination: {
         el: ".swiper-pagination",
-        clickable: true,
-    },
+        clickable: true,        
+    }
 });
+
+var customPagination = document.querySelector('.custom-pagination');
+const menu = ['home','about','skill','project','contact']
+for (var i = 0; i < swiper.slides.length; i++) {
+  var bullet = document.createElement('div');
+  bullet.className = 'custom-bullet';
+  bullet.setAttribute('data-index', i);
+  var txt = document.createTextNode(menu[i])
+  bullet.appendChild(txt)
+  customPagination.appendChild(bullet);
+}
+
+
+customPagination.addEventListener('click', function(e) {
+  index = e.target.getAttribute('data-index');
+  if (index !== null) {
+
+    // 모든 custom-bullet 요소의 색상을 초기화
+    document.querySelectorAll('.custom-bullet').forEach(function(bullet) {
+      bullet.style.color = '#fff'; // 기본 색상으로 설정
+    });
+    // 클릭된 요소의 색상 변경
+    e.target.style.color = '#0090F0'; // 원하는 색상 
+    swiper.slideTo(index);
+  }
+});
+
+
+swiper.on('slideChange',function(){
+  var index=this.activeIndex;  
+  for(let i=0; i<5; i++){
+    document.querySelectorAll('.custom-bullet')[i].style.color = '#fff';
+  }
+  document.querySelectorAll('.custom-bullet')[index].style.color = '#0090F0';
+});
+
 
 // 스와이퍼 프로젝트 페이지
 
